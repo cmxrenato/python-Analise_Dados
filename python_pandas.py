817539017d4a9d540d9e898fa1035dfd65682d7f
cmxrenato <renatoaraujoleal@gmail.com>
2025-08-11 19:18:33 -0300

    selecoes de colunas e valores e somas

--- python_pandas.py
+++ python_pandas.py
@@ -23,19 +23,43 @@ df = pd.read_excel('planilhas/despesas_treinamento.xlsx')
 
 # Filtrar apenas despesas do mês de Fevereiro
 # Filtrar apenas despesas do mês de Fevereiro
-Fevereiro = df[df['Mês'] == 'Fevereiro']
+#Fevereiro = df[df['Ano'] == 'Fevereiro']
 
 # Total por centro de custo
-totais = Fevereiro.groupby('Categoria')['Valor'].sum()
+#totais = Fevereiro.groupby('Categoria')['Valor'].sum()
+#Selecao = df[(df['Ano'] == 2024) & (df['Centro de Custo'] == 'TI')]
+#Selecao = df[(df['Ano'] == 2024)] 
+
+
+#totais = Selecao.groupby('Centro de Custo')['Valor'].sum().reset_index()
+
+
 
 # Calcular percentual de cada centro de custo
-totais = totais.reset_index()
-totais['% do Total'] = (totais['Valor'] / totais['Valor'].sum()) * 100
+#totais = totais.reset_index()
+#totais['% do Total'] = (totais['Valor'] / totais['Valor'].sum()) * 100
+
+
 
 # Exportar relatório para uma pasta de trabalho nova
 #totais.to_excel("relatorio_Fevereiro.xlsx", index=False)
+#totais.to_csv("relatorio_2024-2.csv", index=False)
 
 #criando uma nova planilha na mesma pasta de trabalho
-with pd.ExcelWriter("planilhas/despesas_treinamento.xlsx", engine='openpyxl', mode='a') as writer:
-    totais.to_excel(writer, sheet_name='Resumo Janeiro', index=False)
+#with pd.ExcelWriter("planilhas/despesas_treinamento.xlsx", engine='openpyxl', mode='a') as writer:
+#    totais.to_excel(writer, sheet_name='Resumo Janeiro', index=False)
 
+
+
+# Filtrar despesas do TI em 2024
+ti_2024 = df[(df['Centro de Custo'] == 'TI') & (df['Ano'] == 2024)]
+
+# Mostrar as 5 primeiras linhas para conferir
+print(ti_2024.head())
+
+# Somar total de despesas do TI em 2024
+total_ti_2024 = ti_2024['Valor'].sum()
+print(f"Total de despesas do TI em 2024: R$ {total_ti_2024:.2f}")
+
+# Exportar para Excel
+ti_2024.to_excel('relatorio_TI_2024.xlsx', index=False)
